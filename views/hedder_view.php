0a6ea576736613473a17649d35190360640e4c1c
kazuki19992 <kushida98@gmail.com>
2019-09-03 16:17:46 +0900

    ヘッダーにログイン中のユーザーネームを表示させました

--- views/hedder_view.php
+++ views/hedder_view.php
@@ -4,7 +4,7 @@
         <tr>
             <td><i class="fas fa-star fa-2x"></i><br><a href="#">お気に入り</a></td>
             <td><i class="fas fa-search"></i><br><a href="./search.php">検索</a></td>
-            <td>ユーザー名</td>
+            <td><?php echo html_escape($member['name']); ?></td>
             <td><a href="./views/logout.php">ログアウト</a></td>
         </tr>
     </table>
