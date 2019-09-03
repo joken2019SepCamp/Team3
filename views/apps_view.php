316bc18fae3eb04a5deed6899f763e2f7447bd07
Shogo_Kosuga <shogokosuga@gmail.com>
2019-09-03 15:48:54 +0900

    カード制作しました。

--- views/apps_view.php
+++ views/apps_view.php
@@ -20,12 +20,50 @@
         <!--JavaScript at end of body for optimized loading-->
         <script type="text/javascript" src="js/materialize.min.js"></script>
 
-        <?php require("./views/hedder_view.php"); ?>
-
-        <BR>
-        
         <!-- 投稿ボタン -->
         <a class="btn-floating btn-large waves-effect waves-light red" href="#"><i class="fas fa-utensils"></i></a>
+
+
+
+
+        <div class="col s12 m7">
+    <h2 class="header">おすすめのお店</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="https://tabiiro.jp/lpimg/gourmet/308287/main/img1.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>初めてこんなにおいしいものを食べました。値段も安くお勧めです。</p>
+        </div>
+        <div class="card-action">
+          <a href="#">店舗のリンク</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+            
+
+  <div class="col s12 m7">
+    <h2 class="header">食べたもの</h2>
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="https://shop.r10s.jp/otoshuclub/cabinet/item8/8200097_r2.jpg">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>これまで食べたものの中で一番おいしかったです。ぜひ食べてみてください！</p>
+        </div>
+        <div class="card-action">
+          <a href="#">店舗のリンク</a>
+        </div>
+      </div>
+    </div>
+  </div>
+            
+
     </body>
 </html>
 
