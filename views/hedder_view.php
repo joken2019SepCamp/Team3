81cc9e281370185693bc9631cae668067ed11854
kensin <K25235m!>
2019-09-03 15:40:42 +0900

    ヘッダーをなんかしました

--- views/hedder_view.php
+++ views/hedder_view.php
@@ -1,6 +1,10 @@
 <hedder>
-    <a href="#">お気に入り</a>
-    <a href="#">検索</a>
-    ユーザー名
-    <a href="./logout.php">ログアウト</a>
+    <table border="0">
+        <tr>
+            <td><i class="fas fa-star fa-2x"></i><br><a href="#">お気に入り</a></td>
+            <td><a href="#">検索</a></td>
+            <td>ユーザー名</td>
+            <td><a href="./logout.php">ログアウト</a></td>
+        </tr>
+    </table>
 </hedder>
