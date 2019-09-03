d1c383080277a68316b64081c1be81f93885ae02
kvallend <yuya3610@icloud.com>
2019-09-03 13:06:32 +0900

    ログイン画面の枠組みを作りました

--- views/login_view.php
+++ views/login_view.php
@@ -1,1 +1,27 @@
-<!-- この中にログイン画面のHTMLを書いていきます -->
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>ログイン</title>
+
+    </head>
+    
+    <body>
+        <h1>おかえりなさい</h1>
+        <form action="cgi-bin/aaa.cgi" method="post">
+            <table border="0">
+                <tr>
+                    <td align="right"><b> 学生番号：</b></td>
+                    <td><input type="text" name="name" ></td>
+                </tr>
+                <tr>
+                    <td align="right"><b> パスワード：</b></td>
+                    <td><input type="password" name="pass" ></td>
+                </tr>
+            </table>
+        </form>
+        <p>
+            <input type="submit" value="ログイン">
+        </p>
+    </body>
+</html>
