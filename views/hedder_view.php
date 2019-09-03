ed648db3221b0e99b414d64b93697ada325aa87e
kensin <K25235m!>
2019-09-03 15:49:35 +0900

    ヘッダーでアイコンを付けました！

--- views/hedder_view.php
+++ views/hedder_view.php
@@ -2,7 +2,7 @@
     <table border="0">
         <tr>
             <td><i class="fas fa-star fa-2x"></i><br><a href="#">お気に入り</a></td>
-            <td><a href="#">検索</a></td>
+            <td><i class="fas fa-search"></i><br><a href="#">検索</a></td>
             <td>ユーザー名</td>
             <td><a href="./logout.php">ログアウト</a></td>
         </tr>
