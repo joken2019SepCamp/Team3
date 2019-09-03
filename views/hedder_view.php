fe56ea943d90e0b2c71db216e5b9ee86db98b994
kazuki19992 <kushida98@gmail.com>
2019-09-03 16:19:21 +0900

    ログアウトのリンクを修正しました

--- views/hedder_view.php
+++ views/hedder_view.php
@@ -5,7 +5,7 @@
             <td><i class="fas fa-star fa-2x"></i><br><a href="#">お気に入り</a></td>
             <td><i class="fas fa-search"></i><br><a href="./search.php">検索</a></td>
             <td><?php echo html_escape($member['name']); ?></td>
-            <td><a href="./views/logout.php">ログアウト</a></td>
+            <td><a href="./logout.php">ログアウト</a></td>
         </tr>
     </table>
 </hedder>
