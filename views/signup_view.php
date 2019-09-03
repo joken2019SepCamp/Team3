bb0fe362ddc4e65cca824041d8cf77289234fabf
u306099 <cetk18099@g,nihon-u.ac.jp>
2019-09-03 13:02:41 +0900

    会員登録の枠組みを作りました

--- views/signup_view.php
+++ views/signup_view.php
@@ -1,1 +1,26 @@
 <!-- この中に新規会員登録画面のHTMLを書いていきます -->
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>新規会員登録</title>
+
+  </head>
+
+  <body>
+    <h1>ようこそ！！！</h1>
+
+    <div class="right">
+      ※すべて必須項目です。<br>
+      学籍番号:<input type="text" name="student_num" value=""><br>
+      パスワード：<input type="text" name="password" value=""><br>
+      パスワード(確認用):<input type="text" name="password_check" value=""><br>
+      <br>
+      <input type="button" value="プロフィール画像" onclick="">
+      <input type="button" value="登録" onclick=""><br>
+    </div>
+
+  </body>
+
+
+</html>
