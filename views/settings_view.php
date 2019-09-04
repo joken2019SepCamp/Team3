df3c66d6a66f6d673bef366ebc62af8713ae01b6
kazuki19992 <kushida98@gmail.com>
2019-09-04 14:37:51 +0900

    アカウント設定画面にCSSを適用しました

--- views/settings_view.php
+++ views/settings_view.php
@@ -1,8 +1,21 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
-  <meta charset="UTF-8">
-  <title>アカウント設定</title>
+      <meta charset="UTF-8">
+      <title>アカウント設定</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="./CSS/materialize.min.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+      <!-- fontawesome(アイコン) -->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+
+      <link rel="stylesheet" type="text/css" href="./CSS/style.css">
+      <link rel="stylesheet" type="text/css" href="./CSS/HamburgerMenu.css">
+    </head>
   </head>
 
   <body>
