f51e4aaf6db5ad1e31d7aaddb7ca4fcc468900ec
kazuki19992 <kushida98@gmail.com>
2019-09-05 04:28:51 +0900

    (最終コミット) マスターアップ！！！

--- views/hedder_view.php
+++ views/hedder_view.php
@@ -2,7 +2,7 @@
     <?php require('./views/nav_menu.php'); ?>
     <table border="0">
         <tr border="0">
-            <td><center><a href="#" class="header_lnk"><i class="fas fa-star fa-2x"></i><br>お気に入り</a></center></td>
+            <td><center><a href="./apps.php" class="header_lnk"><i class="fas fa-home fa-2x"></i><br>Home</a></center></td>
             <td><center><a href="./search.php" class="header_lnk"><i class="fas fa-search fa-2x"></i><br>検索</a></center></td>
             <td><center><div class="user_name"><?php echo html_escape($member['name']); ?></div><a href="./logout.php" class="header_lnk">ログアウト</a></center></td>
         </tr>
